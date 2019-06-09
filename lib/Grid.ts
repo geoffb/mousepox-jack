3e8ab01f38176fec29a63bbb191971116499c0eb
Geoff Blair <gmblair@gmail.com>
2019-06-09 17:12:51 -0600

    Add Grid.resize and Grid.forEachInRect

--- lib/Grid.ts
+++ lib/Grid.ts
@@ -1,11 +1,5 @@
-import { IPoint } from "@mousepox/math";
-
-interface IAStarNode {
-  index: number;
-  f: number;
-  g: number;
-  parent?: IAStarNode;
-}
+/** forEach callback signature */
+type GridForEachCallback = (value: number, x: number, y: number) => void;
 
 export class Grid {
 
@@ -22,9 +16,20 @@ export class Grid {
     this.cells.fill(0);
   }
 
+  public resize(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+    this.cells = new Array(width * height);
+    this.cells.fill(0);
+  }
+
   public get(x: number, y: number): number {
-    const index = this.indexFromXY(x, y);
-    return this.cells[index];
+    if (x < 0 || y < 0 || x >= this.width || y >= this.height) {
+      return -1;
+    } else {
+      const index = this.indexFromXY(x, y);
+      return this.cells[index];
+    }
   }
 
   public set(x: number, y: number, value: number) {
@@ -32,38 +37,19 @@ export class Grid {
     this.cells[index] = value;
   }
 
-  public forEach(fn: (value: number, x: number, y: number) => void) {
+  public forEach(fn: GridForEachCallback) {
     const len = this.cells.length;
     for (let i = 0; i < len; ++i) {
       fn(this.cells[i], i % this.width, Math.floor(i / this.width));
     }
   }
 
-  public findPath(ox: number, oy: number, tx: number, ty: number): IPoint[] | undefined {
-
-    // TODO: Find path from ox/oy to tx/ty
-
-    const origin = {
-      f: 0,
-      g: 0,
-      index: this.indexFromXY(ox, oy),
-    };
-
-    const terminus = {
-      f: 0,
-      g: 0,
-      index: this.indexFromXY(tx, ty),
-    };
-
-    const open: IAStarNode[] = [origin];
-    const closed: IAStarNode[] = [];
-
-    while (open.length > 0) {
-      // todo stuff
+  public forEachInRect(x: number, y: number, width: number, height: number, fn: GridForEachCallback) {
+    for (let iy = y; iy < y + height; ++iy) {
+      for (let ix = x; ix < x + width; ++ix) {
+        fn(this.get(ix, iy), ix, iy);
+      }
     }
-
-    // No valid path found
-    return undefined;
   }
 
   private indexFromXY(x: number, y: number): number {
